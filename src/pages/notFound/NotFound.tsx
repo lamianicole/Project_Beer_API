d8ec0f977a2fe2096a836f505358139bf9e4dfcd
Lamia Rizk <lamiarizk@gmx.de>
2025-01-10 16:40:11 +0100

    img path correction

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -4,7 +4,7 @@ const NotFound: React.FC = () => {
     return (
         <div className="not-found-container">
             <div className="image-container">
-            <img src="./img/not-found.png" alt="empty bottle" className="error-image" />
+            <img src="/img/not-found.png" alt="empty bottle" className="error-image" />
                 <h4 className="error-text">We're sorry. Nothing to drink here ...</h4>
             </div>
         </div>
